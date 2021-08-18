7f08f63a614807d54b0e559c2c9a6ab6b4a26a1d
Darcisio Almeida <darcisioalmeida@gmail.com>
2021-08-18 00:42:51 -0300

    Teste Lampada JS

---
+++ lampada_js/script.js
@@ -0,0 +1,11 @@
+
+                function acendeLampada (){
+                document.getElementById("Luz").src= ".//image/lampada-acesa.jpg";
+                }
+                function apagueLampada (){
+                    document.getElementById("Luz").src=".//image/lampada-apagada.jpg";
+                }
+                function quebraLampada () {
+                    document.getElementById("Luz").src=".//image/lampada-quebrada.jpg";
+                }
+           
