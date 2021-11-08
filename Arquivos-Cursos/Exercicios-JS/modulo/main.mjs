80f4135f27389ec8635e2384e47218447d75239f
Darcisio Almeida <darcisioalmeida@gmail.com>
2021-11-08 17:58:12 -0300

    Organizando e Anexando a pasta de Exercicios das Aulas de JS
    Dando continuidade na organização e arquivamento das pastas com exercicios das aulas de JS dos cursos realizados.

---
+++ Arquivos-Cursos/Exercicios-JS/modulo/main.mjs
@@ -0,0 +1,9 @@
+// considerações ao importar coloque sempre " funções entre {} from ./ + nome arquivo"
+
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Marina', 20));
+
+console.log(mostraCidade('Silvia', 'São Paulo'));
+
+console.log(mostraHobby('Carol', 'Ouvir música'));
