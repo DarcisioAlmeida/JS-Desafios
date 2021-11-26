e6e2c41e14755cee43435a3f6616a92d17e5dc6e
Darcisio Almeida <darcisioalmeida@gmail.com>
2021-11-25 22:49:12 -0300

    Organizando e Anexando as pastas das Aulas de JS
    Dando continuidade na organização e arquivamento das pastas com as aulas de JS dos cursos realizados.

---
+++ fluxo_caixa_familiar/fluxo_caixa.js
@@ -0,0 +1,41 @@
+/*
+    Crie um objeto que possuirá 2 propriedades, ambas do tipo array:
+        * receitas (incomes):[]
+        *despesas (expenses):[]
+
+    Agora, crie uma função que ira calcular o total de receitas e despesas e irá mostrar uma mensagem se a  familia está com saldo positivo ou negativo, seguido do valor do saldo
+*/
+
+let family = {
+    incomes:[2500, 3200, 250.43, 360.45],
+    expenses: [320.34, 128.45, 176.87, 1450]
+}
+
+function sum(array) {
+    let total = 0;
+
+    for (let value of array) {
+        total += value
+    }
+
+    return total
+}
+function calculateBalance() {
+    const calculateIncomes = sum(family.incomes)
+    const calculateExpenses = sum(family.expenses)
+
+    const total = calculateIncomes - calculateExpenses
+
+    const itsOk = total >= 0
+
+    let balanceText = "negativo"
+
+    if(itsOk){
+        balanceText = "positivo"
+    }
+
+    console.log(`Seu saldo é ${balanceText}: ${total.toFixed(2)}`)
+}
+
+calculateBalance()
+
